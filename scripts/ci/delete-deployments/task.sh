f527fbb19b142ff71c713c5005e7517ebdc5d3b8
Sabith KS <skarippullilsoopy@pivotal.io>
2016-10-11 17:36:55 -0700

    Made cleanup run in parallel
    Signed-off-by: Angela Chin <achin@pivotal.io>

--- scripts/ci/delete-deployments/task.sh
+++ scripts/ci/delete-deployments/task.sh
@@ -6,7 +6,10 @@ function main() {
   deployments=$(curl -sk https://${BOSH_USER}:${BOSH_PASSWORD}@${BOSH_DIRECTOR}:25555/deployments)
   set -x
 
-  echo "${deployments}" | jq .[].name | grep ${DEPLOYMENTS_WITH_WORD} | xargs -n 1 bosh -t ${BOSH_DIRECTOR} -n delete deployment
+  echo "${deployments}" | jq .[].name | grep ${DEPLOYMENTS_WITH_WORD} | xargs -n 1 -P 5  bosh -t ${BOSH_DIRECTOR} -n delete deployment
+
+  echo "cleaning up orphaned disks and releases"
+  bosh cleanup --all
 }
 
 main
