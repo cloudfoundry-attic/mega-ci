31cc73ade0a155713b3fd20d7b7004210561ff9b
Evan Farrar <efarrar@pivotal.io>
2016-10-31 15:41:56 -0700

    Moving turbulence related tests to a new job
    - Pre compiling etcd-develop for downstream tests
    
    [#133371877]
    
    Signed-off-by: Sabith KS <skarippullilsoopy@pivotal.io>

---
+++ scripts/ci/compile-bosh-release/task-release-from-repo.sh
@@ -0,0 +1,64 @@
+#!/bin/bash -exu
+
+export ROOT="${PWD}"
+export STEMCELL_VERSION=$(cat "${ROOT}"/stemcell/version)
+export RELEASE_VERSION="99999+dev.$(date +%s)"
+
+function main() {
+  upload_stemcell
+  upload_release
+  force_compilation
+}
+
+function force_compilation() {
+  pushd /tmp > /dev/null
+    sed \
+      -e "s/REPLACE_ME_DIRECTOR_UUID/$(bosh -t "${BOSH_DIRECTOR}" status --uuid)/g" \
+      -e "s/REPLACE_ME_RELEASE_VERSION/${RELEASE_VERSION}/g" \
+      -e "s/REPLACE_ME_RELEASE_NAME/${RELEASE_NAME}/g" \
+      "${ROOT}/mega-ci/scripts/ci/compile-bosh-release/fixtures/compilation.yml" > "compilation.yml"
+    bosh -t "${BOSH_DIRECTOR}" -d "/tmp/compilation.yml" -n deploy
+  popd > /dev/null
+
+  pushd "${ROOT}/compiled-bosh-release" > /dev/null
+    bosh -t "${BOSH_DIRECTOR}" -d "/tmp/compilation.yml" export release "${RELEASE_NAME}/${RELEASE_VERSION}" "ubuntu-trusty/${STEMCELL_VERSION}"
+  popd > /dev/null
+
+  bosh -t "${BOSH_DIRECTOR}" -n delete deployment compilation
+}
+
+function upload_stemcell() {
+  pushd "${ROOT}/stemcell" > /dev/null
+    bosh -t "${BOSH_DIRECTOR}" upload stemcell stemcell.tgz --skip-if-exists
+  popd > /dev/null
+}
+
+function upload_release() {
+  pushd "${ROOT}/release-repo" > /dev/null
+    bosh -t "${BOSH_DIRECTOR}" -n create release --name "${RELEASE_NAME}" --force --version "${RELEASE_VERSION}"
+    bosh -t "${BOSH_DIRECTOR}" upload release
+  popd > /dev/null
+}
+
+function cleanup_releases() {
+  bosh -t "${BOSH_DIRECTOR}" -n cleanup
+}
+
+function rollup() {
+  set +x
+  local input
+  input="${1}"
+
+  local output
+
+  IFS=$'\n'
+  for line in ${input}; do
+    output="${output:-""}\n${line}"
+  done
+
+  printf "%s" "${output#'\n'}"
+  set -x
+}
+
+trap cleanup_releases EXIT
+main
